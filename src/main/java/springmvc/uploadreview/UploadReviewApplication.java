371b15ccdf9c456232e1e4901351166a8475e2a9
limslimx3 <saemyung1999@gmail.com>
2024-02-22 21:45:55 +0900

    initial commit

---
+++ src/main/java/springmvc/uploadreview/UploadReviewApplication.java
@@ -0,0 +1,13 @@
+package springmvc.uploadreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadReviewApplication.class, args);
+    }
+
+}
